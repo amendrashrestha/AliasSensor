6af61c8539f7dbc3e3146a6a391b79c5bc2799a3
amendrashrestha <amendra@amendra.win.foi.se>
2018-01-29 15:11:26 +0100

    fixed bug

--- AliasBackend/utilities/IOProperties.py
+++ AliasBackend/utilities/IOProperties.py
@@ -4,7 +4,7 @@ import os
 LIWC_filepath = os.environ['HOME'] + '/repo/AliasSensor/AliasBackend/dictionaries/LIWC/'
 function_word_filepath = os.environ['HOME'] + '/repo/AliasSensor/AliasBackend/dictionaries/function_words/funct.txt'
 
-feature_vector_filepath = os.path.expanduser('~') + "/Desktop/AliasMatching/FV/feature_vector.csv" #feature_vector FV_Sample
+feature_vector_filepath = os.path.expanduser('~') + "/feature_vector.csv" #feature_vector FV_Sample
 feature_vector_sample_filepath = os.path.expanduser('~') + "/Desktop/AliasMatching/FV/FV_Sample.csv" #feature_vector FV_Sample
 
 
