46a59fb37ce8d54be887cd106f1b436b287a70f3
George <amendrashrestha@gmail.com>
2018-02-01 11:03:35 +0100

    new model

--- AliasBackend/main/classifiers.py
+++ AliasBackend/main/classifiers.py
@@ -15,56 +15,47 @@ import AliasBackend.utilities.IOProperties as props
 import AliasBackend.utilities.test_evaluator as eval
 
 def calibratedClassification():
-    fv_dataframe = pd.read_csv(props.englsih_feature_vector_filepath)
+    try:
+        fv_dataframe = pd.read_csv(props.englsih_feature_vector_filepath)
 
-    df = pd.DataFrame(fv_dataframe)
+        df = pd.DataFrame(fv_dataframe)
 
-    abs_result_df = abs(df.diff()).dropna()
-    X = abs_result_df.iloc[:,0:len(abs_result_df.columns)-1]
+        abs_result_df = abs(df.diff()).dropna()
+        X = abs_result_df.iloc[:,0:len(abs_result_df.columns)-1]
 
-    Y = abs_result_df.iloc[:, -1]
+        Y = abs_result_df.iloc[:, -1]
 
-    test_size = 0.2
-    seed = 7
-    kfold = 10
+        test_size = 0.2
+        seed = 7
+        kfold = 10
 
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
-
-    percentiles = [40, 50, 60, 70, 80, 90, 100]
-    for p in percentiles:
-        testing_model_with_SVM(X_train, X_test, Y_train, Y_test, kfold, p)
+        X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
 
 
-def testing_model_with_SVM(x_train, x_test, y_train, y_test, kfold, percentile):
+        testing_model_with_SVM(X_train, X_test, Y_train, Y_test, kfold)
+
+    except Exception:
+        traceback.print_exc()
+
+def testing_model_with_SVM(x_train, x_test, y_train, y_test, kfold):
 
     penalty = 'l2'
     loss = 'squared_hinge'
 
-    svc = LinearSVC(C=1.0, penalty=penalty, loss=loss, dual=True)
-
-    pipeline = Pipeline([
-        ('featureselection', SelectPercentile(chi2, percentile=percentile)),
-        ('model', svc)
-    ])
-
     try:
-        gs = GridSearchCV(estimator=pipeline, cv=kfold)
-        print('\nTraining model with', str(percentile), "% features")
+        svc = LinearSVC(C=1.0, penalty=penalty, loss=loss, dual=True)
+        clf = CalibratedClassifierCV(svc, method="sigmoid")
 
-        clf = CalibratedClassifierCV(svc, method = "sigmoid")
         clf.fit(x_train, y_train)
 
-        best_est = clf.best_estimator_
         #Saving model
-        joblib.dump(best_est, props.english_cal_svm_model_filename)
+        joblib.dump(clf, props.english_cal_svm_model_filename)
 
         print('Testing model')
         # Save the classifiers predictions and create confusion matrix
-        y_test_pred = best_est.predict(x_test)
+        y_test_pred = clf.predict(x_test)
 
-        eval.evaluator(testing_logger_name, [1, 2], [1, 2], y_test, y_test_pred, duration)
-
-        accuracy = best_est.score(x_test, y_test)
+        accuracy = clf.score(x_test, y_test)
         # print(accuracy)
         print("Accuracy: %.2f%%" % (accuracy * 100))
 
